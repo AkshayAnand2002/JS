b840bed68abfe355b7719a327d48f4a205efbebb
Akshay Anand <93818352+AkshayAnand2002@users.noreply.github.com>
2024-05-16 15:49:19 +0000

    test1and2

---
+++ basics/test1.js
@@ -0,0 +1,15 @@
+//console.log("Akshay");
+const accountId=14435
+let accountEmail="akka@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+//accountId=22;  //not allowed
+accountEmail="hc@aa.com"
+accountPassword="54321"
+accountCity="Mumbai"
+let accountstate;
+console.log(accountId);
+console.table([accountId,accountPassword,accountEmail,accountCity,accountstate])
+/*
+prefer not to use var because of issue in block scope and functional scope.
+*/
---
+++ basics/test2.js
@@ -0,0 +1,2 @@
+"use strict";//treat all  JS code as newer version
+//alert("namaste") //we are using nodejs not browser.
